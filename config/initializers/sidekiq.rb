3cee638d67971e5cbe24d9cca4cc059eba979fd9
AndreiMGagiu <gagiumandrei@gmail.com>
2025-06-30 18:21:34 +0300

    chore: integrate Sidekiq Cron to support recurring job scheduling

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -1,5 +1,14 @@
+# frozen_string_literal: true
+
+require 'sidekiq'
+require 'sidekiq-cron'
+
 Sidekiq.configure_server do |config|
   config.redis = { url: ENV.fetch('REDIS_URL', 'redis://localhost:6379/0') }
+
+  schedule_file = Rails.root.join('config/schedule.yml')
+
+  Sidekiq::Cron::Job.load_from_hash YAML.load_file(schedule_file) if File.exist?(schedule_file)
 end
 
 Sidekiq.configure_client do |config|
